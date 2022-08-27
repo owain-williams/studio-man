4c1677a43496bfba41571f52c094452664574485
Owain Williams <owain@andersdigital.com>
2022-08-28 00:47:46 +0100

    added render index

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 "use strict";
 exports.__esModule = true;
 require('dotenv').config();
+// console.log(process.env)
 var express = require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var path = require('path');
 /* Connect to MongoDB Atlas instance. For users that cannot connect to the Production DB, they can run a development instance. */
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/studio_man_app';
 mongoose.connect(mongoURI);
 // Configure Express
 var app = express();
-app.use(bodyParser({ urlEncoded: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, "..", 'views'));
 app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("App listening on port ".concat(port));
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 require('dotenv').config()
+// console.log(process.env)
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 
 /* Connect to MongoDB Atlas instance. For users that cannot connect to the Production DB, they can run a development instance. */
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/studio_man_app'
@@ -10,7 +12,9 @@ mongoose.connect(mongoURI)
 
 // Configure Express
 const app = express()
-app.use(bodyParser({ urlEncoded: true }))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, "..", 'views'));
 app.set('view engine', 'ejs')
 
 // Import custom classes
@@ -30,19 +34,11 @@ import {bookingSchema, bookingModel,
     sessionSchema, sessionModel,
     studioSchema, studioModel} from "./schemas"
 
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
 
 const port = process.env.PORT || 3000
-
-
-
-
-
-
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
